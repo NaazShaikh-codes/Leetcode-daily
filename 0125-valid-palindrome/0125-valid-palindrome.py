b840d85049ce7aabbf96a9d719a7f1a8492d1910
Naaz Shaikh <naazzzhehe88@gmail.com>
2026-09-12 19:38:31 +0530

    Time: 11 ms (31.4%), Space: 19.9 MB (35.06%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        str1 = ""
+
+        for i in s:
+            if i.isalnum():
+                str1 += i.lower()
+
+        if str1 == str1[::-1]:
+            return True
+        else:
+            return False
+        
